158e075a2cce0c41ae75381fd5ff6ffb0bcdfba4
ramsescs <ramses.contreras7@gmail.com>
2023-05-26 00:34:01 +0200

    falta terminar funciones

--- final_falta_funcion_bucle_condicional/MySyntax.py
+++ final_falta_funcion_bucle_condicional/MySyntax.py
@@ -14,17 +14,12 @@ class MySyntax(object):
         print("Construyendo MySyntax")
         self.a = 3
         self.names = []
-        self.tablaSimbolos = {}
-        self.funciones = {}
+        self.funciones = []
         self.variables = {}
-
-        self.variablesI = {}
         self.registros = {}
         self.vectores = {}
-        self.funcionesL = []
-        self.variablesL = []
-        self.registrosL = []
-        self.vectoresL = []
+
+        self.have_control_flow = False
 
         yacc.yacc(module=self)
         yacc.parse(input)
@@ -103,8 +98,8 @@ class MySyntax(object):
     def p_blq_registro(self, p):
         '''blq_registro : tipo varios_name ';' FINPARRAFO blq_registro
                         | tipo varios_name ';'
-                        | VECTOR tipo_VECTOR NAME '[' NUMINTEGER ']' ';' FINPARRAFO blq_registro
-                        | VECTOR tipo_VECTOR NAME '[' NUMINTEGER ']' ';' '''
+                        | VECTOR tipo_vector NAME '[' NUMINTEGER ']' ';' FINPARRAFO blq_registro
+                        | VECTOR tipo_vector NAME '[' NUMINTEGER ']' ';' '''
         valor = 0 if p[1].upper() == 'ENTERO' or p[2].upper() == 'ENTERO' else \
             0.0 if p[1].upper() == 'REAL' or p[2].upper() == 'REAL' else \
                 False if p[1].upper() == 'BOOLEANO' or p[2].upper() == 'BOOLEANO' else ''
@@ -128,15 +123,15 @@ class MySyntax(object):
         print('p_blq_registro', p[0])
 
 
-    def p_tipo_VECTOR (self,p):
-        '''tipo_VECTOR : ENTERO
+    def p_tipo_vector (self,p):
+        '''tipo_vector : ENTERO
                         | REAL
                         | BOOLEANO
                         | CARACTER'''
         p[0] = p[1]
 
     def p_declaracion_vec(self, p):
-        '''declaracion_vec : VECTOR tipo_VECTOR NAME '[' NUMINTEGER ']' '''
+        '''declaracion_vec : VECTOR tipo_vector NAME '[' NUMINTEGER ']' '''
 
         lista = []
         if p[2].upper() == 'ENTERO':
@@ -260,8 +255,42 @@ class MySyntax(object):
 
 
     def p_def_funcion(self, p):
-        '''def_funcion : FUNCION NAME '(' lista_args ')' ':' tipo  '{' FINPARRAFO statements1 DEVOLVER expresion  '}'  '''
+        '''def_funcion : FUNCION NAME '(' lista_args ')' ':' tipo  FINPARRAFO '{' FINPARRAFO statements1 DEVOLVER expresion FINPARRAFO '}'  '''
         # Agrega la acción semántica aquí
+        #TODO: Almacenar en diccionario. Revisar que tipo devuelto == tipo de funcion. Revisar que tipos en la llamada == tipo de parametros definidos
+
+        # Comprobar si exista ya otra funcion con el mismo nombre y argumentos
+        args = self.createArgsDict(p[4])
+
+        if not self.existsFunc(p[2], args):
+            # Agregar dict de funcion a la lista
+            self.funciones.append({'nombre': p[2], 'tipo': p[7], 'lista_args': args})
+
+            print('============= FUNCION ', self.funciones[-1])
+
+        else:
+            print("ERROR6: Funcion ", p[2], " ya declarada con los mismos argumentos")
+
+
+        '''
+        Estructura diccionario:
+        Revisar que tipo devuelto == tipo de funcion
+        **Revisar que tipos en la llamada == tipo de parametros definidos
+
+
+            if p[7].upper() == 'ENTERO' and type(p[13]) == int or p[7].upper() == 'REAL' and type(p[13]) == float \
+                            or p[7].upper() == 'BOOLEANO' and type(p[13]) == bool or \
+                            p[7].upper() == 'CARACTER' and type(p[13]) == str:
+                
+                # Agregar dict de funcion a la lista
+                self.funciones.append({'nombre': p[2], 'tipo': p[7], 'lista_args': args})
+
+                print('============= FUNCION ', self.funciones[p[2]])
+
+            else:
+                print("ERROR: Funcion - Tipo devuelto erroneo")
+
+        '''
 
 
     def p_lista_args(self, p):
@@ -269,20 +298,20 @@ class MySyntax(object):
                       | tipo NAME
                       |'''
         #lista de argumentos que declaramos cuando declaramos una función
-        if len(p) == 2:
-            p[0] = p[1]
-        elif len(p) == 4:
+        if len(p) == 3:
+            p[0] = p[1] + ' ' + p[2]
+        elif len(p) == 5:
             if p[3]:
-                p[0] = p[1] + ',' + p[3]
+                p[0] = p[1] + ' ' + p[2] + ',' + p[4]
             else:
-                p[0] = p[1]
+                p[0] = p[1] + ' ' + p[2]
 
         print("p_lista_args", p[0])
 
 
     def p_expresion(self, p):
-        '''expresion : expresion_and OR expresion
-                     | expresion_and '|' expresion
+        '''expresion : expresion OR expresion_and
+                     | expresion '|' expresion_and
                      | expresion_and'''
         if len(p) == 4:
             p[0] = p[1] or p[3]
@@ -291,8 +320,8 @@ class MySyntax(object):
         print("p_expresion_or", p[0])
 
     def p_expresion_and(self, p):
-        '''expresion_and : expresion_comp AND expresion_and
-                         | expresion_comp '&' expresion_and
+        '''expresion_and : expresion_and AND expresion_comp
+                         | expresion_and '&' expresion_comp
                          | expresion_comp'''
         if len(p) == 4:
             p[0] = p[1] and p[3]
@@ -301,7 +330,7 @@ class MySyntax(object):
         print("p_expresion_and", p[0])
 
     def p_expresion_comp(self, p):
-        '''expresion_comp : expresion_add OPCOMPARE expresion_comp
+        '''expresion_comp : expresion_comp OPCOMPARE expresion_add
                           | expresion_add'''
 
         if len(p) == 4:
@@ -333,15 +362,23 @@ class MySyntax(object):
         print("p_expresion_add", p[0])
 
     def p_expresion_mult(self, p):
-        '''expresion_mult : factor '*' expresion_mult
-                          | factor '/' expresion_mult
+        '''expresion_mult : expresion_mult '*' factor  
+                          | expresion_mult '/' factor
                           | factor'''
 
         if len(p) == 4:
             if p[2] == '*':
                 p[0] = p[1] * p[3]
             elif p[2] == '/':
-                p[0] = p[1] / p[3]
+                result = p[1] / p[3]
+
+                # Result is a float
+                if result != int(result):
+                    p[0] = result
+                # Result is an int
+                else:
+                    p[0] = int(result)
+
         else:
             p[0] = p[1]
         print("p_expresion_mult", p[0])
@@ -406,6 +443,7 @@ class MySyntax(object):
                  | FALSO
                  | CARACTERC
                  | name_chain
+                 | name_chain '[' expresion ']'
                  | name_chain '.' LONG '''
         #siempre devolverá un número o un boolean o un caracter, numca un name
 
@@ -444,22 +482,53 @@ class MySyntax(object):
 
 
         elif len(p) == 4:
-            #name_chain '.' LONG; tamaño del vector, buscar la biblioteca de vectores y obtener su tamaño
-            pass
+            if p[1] in self.vectores and p[3].upper() == 'LONG' and '.' not in p[1]:
+                p[0] = self.vectores[p[1]]["tam"]
+            elif p[3].upper() == 'LONG' and '.' in p[1]:
+
+                names = p[1].split('.')
+
+                hay = True
+                name = self.variables[names[0]]
+                for a in range(1, len(names)):
+                    if names[a] in name['valor']:
+                        name = name['valor'][names[a]]
+                    else:
+                        print("No existe el registro ", p[1] , " que intenta acceder")
+                        hay = False
+                if hay:
+                    p[0] = name["tam"]
+
+        elif len(p) == 5:
+            if p[1] in self.vectores:
+                if type(p[3]) == int and p[3]<self.vectores[p[1]]['tam']:
+                    p[0] = self.vectores[p[1]]['valor'][p[3]]
+                else:
+                        print("ERROR17: Imposible acceder a esa posición.")
+            else:
+                print("ERROR18: Vector ", p[1], "no declarada")
 
 
         print("p_valor", p[0])
 
     def p_condicional(self, p):
-        '''condicional : SI expresion FINPARRAFO ENTONCES FINPARRAFO statements1 SINO FINPARRAFO statements1  FINSI
+        '''condicional : SI expresion FINPARRAFO ENTONCES FINPARRAFO statements1 SINO FINPARRAFO statements1 FINSI
                     | SI expresion FINPARRAFO ENTONCES FINPARRAFO statements1 FINSI'''
-        # Agrega la acción semántica aquí
+        if type(p[2]) == bool:
+            self.have_control_flow = True
+            print(p[2])
+        else:
+            print('ERROR23: Error en la condicion, no es un booleano')
 
 
     def p_bucle(self, p):
         '''bucle : MIENTRAS expresion statements1 FINMIENTRAS
         '''
-        # Agrega la acción semántica aquí
+        if type(p[2]) == bool:
+            self.have_control_flow = True
+            print(p[2])
+        else:
+            print('ERROR23: Error en la condicion, no es un booleano')
 
     def p_name_chain(self, p):
         '''name_chain : NAME
@@ -508,5 +577,38 @@ class MySyntax(object):
             return dic
         else:
             return nombres
+        
+    def haveControlFlow(self):
+        return self.have_control_flow
+    
+    def haveFunctions(self):
+        return bool(self.funciones)
+    
+    def existsFunc(self, fName, args):
 
+        for func in self.funciones:
+            if(func['nombre'] == fName):
+                # Comprobar si coinciden numero de args
+                if len(args.keys()) == len(func['lista_args'].keys()):
+                    # Comprobar si los tipos son iguales
+                    same_types = True
+                    arg_names = func['lista_args'].keys()
+                    for i in range(len(args.keys())):
+                        if args[args.keys()[i]].upper() != func['lista_args'][arg_names[i]].upper():
+                            same_types = False
+
+                    # Funcion encontrada con nombre y tipos iguales
+                    if same_types: return True
+        
+        return False
+            
+    def createArgsDict(self, args):
+        dict = {}
+        for arg in args.split(','):
+            arg_name = arg.split(' ')[1]
+            arg_type = arg.split(' ')[0]
+
+            dict[arg_name] = arg_type
+
+        return dict
 
--- final_falta_funcion_bucle_condicional/main.py
+++ final_falta_funcion_bucle_condicional/main.py
@@ -1,15 +1,32 @@
 import sys
+import os
 from MyLexer import MyLexer
 sys.path.insert(0, "../..")
 from MySyntax import MySyntax
 import ply.yacc as yacc
+import json
 
-fname="input.txt"
+fname="./pruebas/correcto-test_1_entero"
 f= open(fname, 'r')
 s = f.read()
-MySyntax(s)
+p = MySyntax(s)
 
-"""myLex = MyLexer()
+json_dict = {}
+for var in p.variables.keys():
+    json_dict[var] = {'tipo': p.variables[var]['tipo']}
+
+    if not (p.haveControlFlow() or p.haveFunctions()):
+        json_dict[var]['valor'] = p.variables[var]['valor']
+
+fname = 'output.json'
+
+with open(fname, 'w') as file:
+    # Write the dictionary as JSON to the file
+    json.dump(json_dict, file)
+
+
+"""
+myLex = MyLexer()
 lexer = myLex.lexer
 lexer.input(f.read())
 myLex.test(s)
